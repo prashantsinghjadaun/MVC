003bf8cc03bf9bc05af018736242b4f2b81c6168
prashantsinghjadaun <jadaunprashant777@gmail.com>
2026-03-04 22:35:16 +0530

    Added about page view for MVC project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MVC Project</title>
+</head>
+<body>
+
+<h1>About MVC Practice Project</h1>
+
+<p> Created to learn Laravel MVC framework</p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 
 Route::view('/table', 'table');
+Route::view('/about', 'about');
