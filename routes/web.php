0c201e9072109bbb14993db2a6a10add9ff56d87
prashantsinghjadaun <jadaunprashant777@gmail.com>
2026-03-04 22:20:14 +0530

    Fixed routing and added table view route

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/', function() {
-    return "Hello World";
-    echo("hi ");
-});
-
-// direct method to fetch the view 
 Route::view('/', 'welcome');
 
-Route::view('/', 'table');
+Route::view('/table', 'table');
