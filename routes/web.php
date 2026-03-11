9f08ebf614b977d0dd38030381be7ea05ab50989
prashantsinghjadaun <jadaunprashant777@gmail.com>
2026-03-11 20:52:34 +0530

    Cleaned route conflicts and improved parameter routing

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::view('/', 'welcome');
+Route::get('/greet', function () {
+    return view('welcome');
+});
 
-Route::view('/table', 'table');
-Route::view('/about', 'about');
-Route::view('/contact', 'contact');
+Route::get("/",function(){
+    return view('student');
+});
+
+Route::get("/student",function(){
+    return view('student');
+});
+
+Route::view('/about',"about");
+
+// Route parameters
+Route::get("/params/{id}",function($id){
+    return "<h1>Id passed is $id</h1>";
+});
+
+// optional parameter
+Route::get("/params/{id?}",function($id=null){
+    return "<h1>Id passed is $id</h1>";
+});
+
+// multiplication table
+Route::get("/multiply/{id}",function($id){
+    for($i=1;$i<=10;$i++){
+        echo "$id X $i = ".$id*$i."<br>";
+    }
+})->whereNumber('id');
+
+// Controller route
+Route::get('/table', [PageController::class, 'table']);
