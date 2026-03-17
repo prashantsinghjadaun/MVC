19a121db19087c430042e7493b4e8b8639a08db7
prashantsinghjadaun <jadaunprashant777@gmail.com>
2026-03-17 13:41:48 +0530

    Added navigation layout and created about page using Blade layout system

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,21 @@
 
 <h1>Header - MVC Project</h1>
 
+<h2>TEST NAVBAR POSITION</h2>
+
+<nav>
+    <a href="/">Home</a> |
+    <a href="/student">Student</a> |
+    <a href="/about">About</a> |
+    <a href="/contact">Contact</a>
+</nav>
+
+<hr>
+
 @yield('content')
 
 <hr>
+
 <p>Footer - 2026</p>
 
 </body>
